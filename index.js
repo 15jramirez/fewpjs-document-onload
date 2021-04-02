c54fb801e869ea67e96b1e255260c62039b7b729
jramirez <ramirez.jocelyn73@yahoo.com>
2021-04-01 22:36:37 -0700

    commit tree e6fb46319f346e311dae35b2108cd2a242b976ee

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDom();
+  })
+
+function updateDom(){
+    let newChange = document.getElementById("text");
+
+    newChange.innerHTML = "This is really cool!";
+}
